c9fd42359487d13819ca581b934b7527113c64bc
Starenesha <olgastarynenko@gmail.com>
2020-05-12 16:22:09 +0300

    add article_image field into Post modele

--- blog/models.py
+++ blog/models.py
@@ -2,23 +2,24 @@ from django.db import models
 from django.shortcuts import reverse
 from django.utils.text import slugify
 from time import time
-
+from ckeditor.fields import RichTextField
 
 def gen_slug(s):
     new_slug = slugify(s, allow_unicode=True)
-    return new_slug +"-"+str(int(time()))
+    return new_slug + "-" + str(int(time()))
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True )
     slug = models.SlugField(max_length=150, blank=True, unique=True)
-    body = models.TextField(blank=True, db_index=True)
+    body = RichTextField()
     date_pub = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('Tag',related_name='posts',
-                                  blank=True)  # blank=True означает что это поле необязательное к заполнению
+                                  blank=True)
+    article_image = models.FileField(blank=True, null=True, verbose_name="attachment")
 
     def get_absolute_url(self):
-        return reverse('post_detail_url',kwargs={'slug':self.slug})
+        return reverse('post_detail_url', kwargs={'slug':self.slug})
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -56,67 +57,4 @@ class Tag(models.Model):
 
     class Meta:
         ordering = ['title']
-
-from django.contrib.auth.models import BaseUserManager
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
-from django.utils.translation import ugettext_lazy as _
-
-class MyUserManager(BaseUserManager):
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        if not email:
-            raise ValueError('The Email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-        def create_user(self, email, password=None, **extra_fields):
-            """Create and save a regular User with the given email and password."""
-            extra_fields.setdefault('is_staff', False)
-            extra_fields.setdefault('is_superuser', False)
-            return self._create_user(email, password, **extra_fields)
-
-        def create_superuser(self, email, password, **extra_fields):
-            extra_fields.setdefault('is_staff', True)
-            extra_fields.setdefault('is_superuser', True)
-            extra_fields.setdefault('is_active', True)
-
-            if extra_fields.get('is_staff') is not True:
-                raise ValueError('Superuser must have is_staff=True.')
-            if extra_fields.get('is_superuser') is not True:
-                raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True, null=True)
-    is_staff = models.BooleanField(
-        _('staff status'),
-        default=False,
-        help_text=_('Designates whether the user can log into this site.'),
-    )
-    is_active = models.BooleanField(
-        _('active'),
-        default=True,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
-    USERNAME_FIELD = 'email'
-    objects = MyUserManager()
-
-    def __str__(self):
-        return self.email
-
-    def get_full_name(self):
-        return self.email
-
-    def get_short_name(self):
-        return self.email
 
--- blog/utils.py
+++ blog/utils.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from django.urls import reverse
 
+
 class ObjectDetailMixin:
 
     model = None
     template = None
 
-    def get(self, request,slug):
-        obj = get_object_or_404(self.model,slug__exact=slug)
-        return render(request, self.template, context={self.model.__name__.lower(): obj,'admin_obj':obj,'deteil':True})
+    def get(self, request, slug):
+        obj = get_object_or_404(self.model, slug__exact=slug)
+        return render(request, self.template, context={self.model.__name__.lower(): obj, 'admin_obj':obj, 'detail':True})
 
 
 class ObjectCreateMixin:
@@ -18,16 +19,16 @@ class ObjectCreateMixin:
 
     def get(self, request):
         form = self.form_model()
-        return render(request, self.template,context={'form':form})
+        return render(request, self.template, context={'form':form})
 
     def post(self,request):
-        bound_form = self.form_model(request.POST)
+        bound_form = self.form_model(request.POST or None,request.FILES or None)
 
         if bound_form.is_valid():
             new_obj = bound_form.save()
             return redirect(new_obj)
         else:
-            return render(request, self.template, context={'form':bound_form})
+            return render(request, self.template, context={'form': bound_form})
 
 
 class ObjectUpdateMixin:
@@ -38,30 +39,29 @@ class ObjectUpdateMixin:
     def get(self,request,slug):
         obj = self.model.objects.get(slug__iexact=slug)
         bound_form = self.form_model(instance=obj)
-        return render(request, self.template, context={'form':bound_form,'tag':obj})
+        return render(request, self.template, context={'form':bound_form, 'tag':obj})
 
     def post(self,request,slug):
         obj = self.model.objects.get(slug__iexact=slug)
-        bound_form = self.form_model(request.POST,instance=obj)
+        bound_form = self.form_model(request.POST, instance=obj)
 
         if bound_form.is_valid():
             new_obj = bound_form.save()
             return redirect(new_obj)
         else:
-            return render(request, self.template, context={'form':bound_form,self.__name__.lower():obj})
-
+            return render(request, self.template, context={'form':bound_form, self.__name__.lower(): obj})
 
 
 class ObjectDeleteMixin:
     model = None
     template = None
-    redirect_url  =None
+    redirect_url = None
 
-    def get(self,request,slug):
+    def get(self, request, slug):
         obj = self.model.objects.get(slug__iexact=slug)
         return render(request, self.template, context={self.model.__name__.lower():obj})
 
-    def post(self,request,slug):
+    def post(self, request, slug):
         obj = self.model.objects.get(slug__iexact=slug)
         obj.delete()
         return redirect(reverse(self.redirect_url))
